dab1fc372e6e07d6fd93490a78c6f4f805b5cba6
Ruslan Dzyuba <ruslick.ua@gmail.com>
2025-10-26 19:45:13 +0100

    Create worksiteHandler.js

---
+++ js/pdf/worksiteHandler.js
@@ -0,0 +1,19 @@
+// worksiteHandler.js
+import { genPDF } from './genPDF.js';
+import { compilePDF } from './compilePDF.js';
+
+export const downloadWorksite = async (prototype) => {
+  if (prototype.prototypeType === 'generate') {
+    genPDF(prototype);
+  } else if (prototype.prototypeType === 'pre_filled') {
+
+    const res = await fetch(`./prototypes/cantiere_enel_pre_filled.json`);
+    let prototypeData = null;
+    if (res.ok) prototypeData = await res.json();
+    compilePDF(prototypeData);
+
+    //compilePDF(prototype);
+  } else {
+    console.warn('Tipo prototipo non riconosciuto');
+  }
+};
